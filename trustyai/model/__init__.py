7087aa15caf2792985f8f5c84edc91298a252d89
Rui Vieira <ruidevieira@googlemail.com>
2021-04-30 22:59:54 +0100

    Add feature linearisation

--- trustyai/model/__init__.py
+++ trustyai/model/__init__.py
@@ -2,6 +2,10 @@
 """General model classes"""
 from org.kie.kogito.explainability.model import (
     PerturbationContext as _PerturbationContext,
+    Feature as _Feature,
+    FeatureFactory as _FeatureFactory,
 )
 
 PerturbationContext = _PerturbationContext
+Feature = _Feature
+FeatureFactory = _FeatureFactory
--- trustyai/utils/DataUtils.py
+++ trustyai/utils/DataUtils.py
@@ -11,9 +11,16 @@ euclideanDistance = du.euclideanDistance
 hammingDistance = du.hammingDistance
 doublesToFeatures = du.doublesToFeatures
 exponentialSmoothingKernel = du.exponentialSmoothingKernel
+generateRandomDataDistribution = du.generateRandomDataDistribution
 
 
 def perturbFeatures(originalFeatures, perturbationContext):
     """Perform perturbations on a fixed number of features in the given input."""
     jlist = ArrayList(originalFeatures)
     return du.perturbFeatures(jlist, perturbationContext)
+
+
+def getLinearizedFeatures(originalFeatures):
+    """Transform a list of eventually composite / nested features into a flat list of non composite / non nested features."""
+    jlist = ArrayList(originalFeatures)
+    return du.getLinearizedFeatures(jlist)
