c4c479e48f998593de7623cf16201011d9d347cb
Rui Vieira <ruidevieira@googlemail.com>
2021-07-02 11:42:44 +0100

    Add counterfactual test
    Add implicit converters to List and UUID
    
    Reformat test

--- deps.sh
+++ deps.sh
@@ -2,8 +2,8 @@
 
 mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:get \
     -DremoteRepositories=https://repository.sonatype.org/content/repositories/central  \
-    -Dartifact=org.kie.kogito:explainability-core:1.5.0.Final \
+    -Dartifact=org.kie.kogito:explainability-core:1.8.0.Final \
     -Dmaven.repo.local=dep -q
 
 # We also need the test JARs in order to get the test models
-wget -O ./dep/org/kie/kogito/explainability-core/1.5.0.Final/explainability-core-1.5.0.Final-tests.jar https://repo1.maven.org/maven2/org/kie/kogito/explainability-core/1.5.0.Final/explainability-core-1.5.0.Final-tests.jar
+wget -O ./dep/org/kie/kogito/explainability-core/1.8.0.Final/explainability-core-1.8.0.Final-tests.jar https://repo1.maven.org/maven2/org/kie/kogito/explainability-core/1.8.0.Final/explainability-core-1.8.0.Final-tests.jar
---
+++ trustyai/local/counterfactual/__init__.py
@@ -0,0 +1,9 @@
+# pylint: disable = import-error
+"""Counterfactual explanations"""
+from org.kie.kogito.explainability.local.counterfactual import (
+    CounterfactualExplainer as _CounterfactualExplainer,
+    CounterfactualConfigurationFactory as _CounterfactualConfigurationFactory
+)
+
+CounterfactualExplainer = _CounterfactualExplainer
+CounterfactualConfigurationFactory = _CounterfactualConfigurationFactory
