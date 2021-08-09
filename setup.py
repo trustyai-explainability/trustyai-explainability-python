394af57b50244660c45165114c99f5e0cb2a0216
Rui Vieira <ruidevieira@googlemail.com>
2021-08-09 22:14:41 +0100

    Prepare for PyPi release

--- setup.py
+++ setup.py
@@ -1,3 +1,32 @@
-import setuptools
+import os
+from setuptools import setup
 
-setuptools.setup(packages=['trustyai', 'trustyai.model', 'trustyai.utils'])
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="trustyai",
+    version="0.0.2",
+    description="Python bindings to the TrustyAI explainability library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/trustyai-python/module",
+    author="Rui Vieira",
+    author_email="rui@redhat.com",
+    license='Apache License 2.0',
+    platforms='any',
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Java",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Java Libraries"
+    ],
+    packages=['trustyai', 'trustyai.model', 'trustyai.utils'],
+    include_package_data=False,
+    install_requires=['Jpype1'],
+)
