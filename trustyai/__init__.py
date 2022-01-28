01f5e92d6fa970f759561e87bd97d862bdfb2aee
Rui Vieira <ruidevieira@googlemail.com>
2022-01-28 13:27:42 +0000

    Fix formatting

--- trustyai/__init__.py
+++ trustyai/__init__.py
@@ -1,12 +1,12 @@
 # pylint: disable = import-error, import-outside-toplevel, dangerous-default-value, invalid-name, R0801
 """Main TrustyAI Python bindings"""
 from typing import List
+import site
+import os
 import uuid
 import jpype
 import jpype.imports
 from jpype import _jcustomizer, _jclass
-import site
-import os
 
 TRUSTY_VERSION = "1.12.0.Final"
 DEFAULT_DEP_PATH = os.path.join(site.getsitepackages()[0], "trustyai", "dep")
