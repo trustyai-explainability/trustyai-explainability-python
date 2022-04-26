17ac4760eef0cc5ba625a5675f1f1af89c31bf5f
Rui Vieira <ruidevieira@googlemail.com>
2022-04-26 21:38:15 +0100

    Add theme support for plots

--- trustyai/utils/_visualisation.py
+++ trustyai/utils/_visualisation.py
@@ -8,3 +8,10 @@ class ExplanationVisualiser(ABC):
     def as_dataframe(self) -> pd.DataFrame:
         """Display explanation result as a dataframe"""
         pass
+
+
+DEFAULT_STYLE = {
+    "positive_primary_colour": "#13ba3c",
+    "negative_primary_colour": "#ee0000",
+    "neutral_primary_colour": "#ffffff"
+}
