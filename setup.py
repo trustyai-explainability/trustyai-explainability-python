be7a99a19c1858185afe36fc03f5f0e09e18d6d1
Rob Geada <rob@geada.net>
2022-07-20 14:31:50 +0100

    Added distutils workaround to sitepackages virtualenv bug

--- setup.py
+++ setup.py
@@ -15,7 +15,11 @@ class PostInstall(install):
 
     def run(self):
         install.run(self)
-        _ROOT = os.path.join(site.getsitepackages()[0], "trustyai", "dep")
+        try:
+            _ROOT = os.path.join(site.getsitepackages()[0], "trustyai", "dep")
+        except AttributeError:
+            from distutils.sysconfig import get_python_lib
+            _ROOT = os.path.join(get_python_lib(), "trustyai", "dep")
         print(f"Installing Maven dependencies into {_ROOT}")
         os.system(f"mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:get "
                   f"-DremoteRepositories=https://repository.sonatype.org/content/repositories/central  "
