3f2dc5be3e0cbb7f860eb23ec41e4a85120a3d14
Rob Geada <rob@geada.net>
2022-07-20 14:31:50 +0100

    Added distutils workaround to sitepackages virtualenv bug

--- trustyai/__init__.py
+++ trustyai/__init__.py
@@ -13,7 +13,11 @@ import jpype.imports
 from jpype import _jcustomizer, _jclass
 
 TRUSTY_VERSION = "1.22.1.Final"
-DEFAULT_DEP_PATH = os.path.join(site.getsitepackages()[0], "trustyai", "dep")
+try:
+    DEFAULT_DEP_PATH = os.path.join(site.getsitepackages()[0], "trustyai", "dep")
+except AttributeError:
+    from distutils.sysconfig import get_python_lib
+    DEFAULT_DEP_PATH = os.path.join(get_python_lib(), "trustyai", "dep")
 print(DEFAULT_DEP_PATH)
 
 CORE_DEPS = [
