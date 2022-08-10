6526fca51e3a05f54fa5ee52225ba9252b5f6e0f
Rui Vieira <ruidevieira@googlemail.com>
2022-08-10 13:00:36 +0100

    Update explainers and initialiser

--- trustyai/metrics/__init__.py
+++ trustyai/metrics/__init__.py
@@ -1,7 +1,7 @@
 # pylint: disable = import-error, invalid-name, wrong-import-order
 """General model classes"""
 from trustyai import _default_initializer
-from org.kie.kogito.explainability.utils import (
+from org.kie.trustyai.explainability.utils import (
     ExplainabilityMetrics as _ExplainabilityMetrics,
 )
 
