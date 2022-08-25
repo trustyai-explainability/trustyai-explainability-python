399f90fa8bdb84f68f91ed9cc9d43abc7a01d427
Rui Vieira <ruidevieira@googlemail.com>
2022-08-25 18:29:33 +0100

    Move pylint exception to top-level

--- trustyai/metrics/__init__.py
+++ trustyai/metrics/__init__.py
@@ -1,8 +1,6 @@
-# pylint: disable = import-error, invalid-name, wrong-import-order
+# pylint: disable = import-error, invalid-name, wrong-import-order, no-name-in-module
 """General model classes"""
-from trustyai import (
-    _default_initializer,
-)  # pylint: disable=unused-import, no-name-in-module
+from trustyai import _default_initializer  # pylint: disable=unused-import
 from org.kie.kogito.explainability.utils import (
     ExplainabilityMetrics as _ExplainabilityMetrics,
 )
