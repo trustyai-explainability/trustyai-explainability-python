11355335ab348e9fe25d1ae116343fea4911869e
Rui Vieira <ruidevieira@googlemail.com>
2022-08-25 18:25:34 +0100

    Bypass pylint initializer import error

--- trustyai/metrics/__init__.py
+++ trustyai/metrics/__init__.py
@@ -1,6 +1,8 @@
 # pylint: disable = import-error, invalid-name, wrong-import-order
 """General model classes"""
-from trustyai import _default_initializer  # pylint: disable=unused-import
+from trustyai import (
+    _default_initializer,
+)  # pylint: disable=unused-import, no-name-in-module
 from org.kie.kogito.explainability.utils import (
     ExplainabilityMetrics as _ExplainabilityMetrics,
 )
