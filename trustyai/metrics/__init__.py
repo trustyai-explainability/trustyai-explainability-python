7293b415049a8ac764ed36d2b0416bf49060f95d
Rui Vieira <ruidevieira@googlemail.com>
2022-08-25 18:20:13 +0100

    Revert metrics initializer import

--- trustyai/metrics/__init__.py
+++ trustyai/metrics/__init__.py
@@ -1,6 +1,6 @@
 # pylint: disable = import-error, invalid-name, wrong-import-order
 """General model classes"""
-from trustyai._default_initializer import *  # pylint: disable=unused-import
+from trustyai import _default_initializer  # pylint: disable=unused-import
 from org.kie.kogito.explainability.utils import (
     ExplainabilityMetrics as _ExplainabilityMetrics,
 )
