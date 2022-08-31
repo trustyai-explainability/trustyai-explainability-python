34dd9f2e6afd18b65b7f4c400c9174dadec38fb8
Rui Vieira <ruidevieira@googlemail.com>
2022-08-31 13:34:25 +0100

    Fix benchmark call

--- tests/benchmarks/benchmark.py
+++ tests/benchmarks/benchmark.py
@@ -1,4 +1,3 @@
-
 # pylint: disable=R0801
 """Common methods and models for tests"""
 import os
@@ -17,7 +16,7 @@ from trustyai.model import (
 )
 
 
-def mock_feature(value, name='f-num'):
+def mock_feature(value, name="f-num"):
     """Create a mock numerical feature"""
     return FeatureFactory.newNumericalFeature(name, value)
 
@@ -25,6 +24,8 @@ def mock_feature(value, name='f-num'):
 def sum_skip_model(inputs: np.ndarray) -> np.ndarray:
     """SumSkip test model"""
     return np.sum(inputs[:, [i for i in range(inputs.shape[1]) if i != 5]], 1)
+
+
 import pytest
 
 # pylint: disable=import-error, wrong-import-position, wrong-import-order, R0801
@@ -35,7 +36,9 @@ from pytest import approx
 
 from trustyai.explainers import CounterfactualExplainer
 from trustyai.model import (
-    output, Model, feature,
+    output,
+    Model,
+    feature,
 )
 from trustyai.utils import TestUtils
 
@@ -56,10 +59,7 @@ def non_empty_input():
 
     model = TestUtils.getSumSkipModel(0)
 
-    counterfactual_result = explainer.explain(
-        inputs=features,
-        goal=goal,
-        model=model)
+    counterfactual_result = explainer.explain(inputs=features, goal=goal, model=model)
     for entity in counterfactual_result._result.entities:
         print(entity)
         assert entity is not None
@@ -70,7 +70,8 @@ def counterfactual_match():
     goal = [output(name="inside", dtype="bool", value=True, score=0.0)]
 
     features = [
-        feature(name=f"f-num{i + 1}", value=10.0, dtype="number", domain=(0.0, 1000.0)) for i in range(4)
+        feature(name=f"f-num{i + 1}", value=10.0, dtype="number", domain=(0.0, 1000.0))
+        for i in range(4)
     ]
 
     center = 500.0
@@ -79,10 +80,7 @@ def counterfactual_match():
     explainer = CounterfactualExplainer(steps=10000)
 
     model = TestUtils.getSumThresholdModel(center, epsilon)
-    result = explainer.explain(
-        inputs=features,
-        goal=goal,
-        model=model)
+    result = explainer.explain(inputs=features, goal=goal, model=model)
 
     total_sum = 0
     for entity in result._result.entities:
@@ -104,18 +102,18 @@ def counterfactual_match_python_model():
     n_features = 5
 
     features = [
-        feature(name=f"f-num{i + 1}", value=10.0, dtype="number", domain=(0.0, 1000.0)) for i in range(n_features)
+        feature(name=f"f-num{i + 1}", value=10.0, dtype="number", domain=(0.0, 1000.0))
+        for i in range(n_features)
     ]
     explainer = CounterfactualExplainer(steps=1000)
 
-    model = Model(sum_skip_model, dataframe=False, output_names=['sum-but-5'])
+    model = Model(sum_skip_model, dataframe=False, output_names=["sum-but-5"])
 
-    result = explainer.explain(
-        inputs=features,
-        goal=goal,
-        model=model)
+    result = explainer.explain(inputs=features, goal=goal, model=model)
 
-    assert sum([entity.as_feature().value.as_number() for entity in result._result.entities]) == approx(GOAL_VALUE, rel=3)
+    assert sum(
+        [entity.as_feature().value.as_number() for entity in result._result.entities]
+    ) == approx(GOAL_VALUE, rel=3)
 
 
 def counterfactual_plot():
@@ -125,37 +123,36 @@ def counterfactual_plot():
     n_features = 5
 
     features = [
-        feature(name=f"f-num{i + 1}", value=10.0, dtype="number", domain=(0.0, 1000.0)) for i in range(n_features)
+        feature(name=f"f-num{i + 1}", value=10.0, dtype="number", domain=(0.0, 1000.0))
+        for i in range(n_features)
     ]
     explainer = CounterfactualExplainer(steps=1000)
 
-    model = Model(sum_skip_model, dataframe=False, output_names=['sum-but-5'])
+    model = Model(sum_skip_model, dataframe=False, output_names=["sum-but-5"])
 
-    result = explainer.explain(
-        inputs=features,
-        goal=goal,
-        model=model)
-    
+    result = explainer.explain(inputs=features, goal=goal, model=model)
+
     result.plot()
 
 
 def counterfactual_v2():
     np.random.seed(0)
     data = pd.DataFrame(np.random.rand(1, 5))
-    features = [feature(str(k), "number", v, domain=(-10., 10.)) for k, v in data.iloc[0].items()]
+    features = [
+        feature(str(k), "number", v, domain=(-10.0, 10.0))
+        for k, v in data.iloc[0].items()
+    ]
     model_weights = np.random.rand(5)
     predict_function = lambda x: np.dot(x.values, model_weights)
 
     model = Model(predict_function, dataframe=True)
     goal = np.array([[0]])
     explainer = CounterfactualExplainer(steps=10_000)
-    explanation = explainer.explain(
-        inputs=features,
-        goal=goal,
-        model=model)
+    explanation = explainer.explain(inputs=features, goal=goal, model=model)
     result_output = model(explanation.proposed_features_dataframe)
-    assert result_output<.01
-    assert result_output>-.01
+    assert result_output < 0.01
+    assert result_output > -0.01
+
 
 def test_fib_10(benchmark):
-    benchmark(counterfactual_match, 5)
+    benchmark(counterfactual_match)
