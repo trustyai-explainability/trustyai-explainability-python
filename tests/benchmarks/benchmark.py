f26af11ba6a2b2cd3f049ab828964eecd66b3863
Rui Vieira <ruidevieira@googlemail.com>
2022-08-31 20:57:15 +0100

    Add LIME benchmark

--- tests/benchmarks/benchmark.py
+++ tests/benchmarks/benchmark.py
@@ -9,6 +9,7 @@ myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + "/../general/")
 
 import test_counterfactualexplainer as tcf
+import test_limeexplainer as tlime
 
 
 @pytest.mark.benchmark(
@@ -33,3 +34,10 @@ def test_non_empty_input(benchmark):
 def test_counterfactual_match_python_model(benchmark):
     """Counterfactual match (Python model)"""
     benchmark(tcf.test_counterfactual_match_python_model)
+
+@pytest.mark.benchmark(
+    group="lime", min_rounds=10, timer=time.time, disable_gc=True, warmup=True
+)
+def test_non_empty_input(benchmark):
+    """Counterfactual match (Python model)"""
+    benchmark(tlime.test_non_empty_input)
