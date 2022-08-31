e3da090ac1b38d0e4743dfe5f14bacf0f38e47d1
Rui Vieira <ruidevieira@googlemail.com>
2022-08-31 15:47:52 +0100

    Change benchmark name

--- tests/benchmarks/benchmark.py
+++ tests/benchmarks/benchmark.py
@@ -20,7 +20,7 @@ def test_counterfactual_match(benchmark):
 
 
 @pytest.mark.benchmark(
-    group="counterfactuals", min_rounds=5, timer=time.time, disable_gc=True, warmup=True
+    group="counterfactuals", min_rounds=5, timer=time.time, disable_gc=True, warmup=True, name="Non-empty input"
 )
 def test_non_empty_input(benchmark):
     """Counterfactual non-empty input"""
