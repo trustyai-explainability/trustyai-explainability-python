4d9b089a6ffb4017be2c41076aa7112e6fc51f83
Rui Vieira <ruidevieira@googlemail.com>
2022-10-11 13:43:22 +0100

    Add path to tests

--- tests/general/common.py
+++ tests/general/common.py
@@ -5,6 +5,9 @@ import sys
 import numpy as np
 import pandas as pd  # pylint: disable=unused-import
 
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + "/../../src")
+
 from trustyai.model import (
     FeatureFactory,
 )
