e8cd4845db836d3efbd6bcae3cce463e89c14394
Rob Geada <rob@geada.net>
2022-11-21 10:56:58 +0000

    FAI-849: Tyrus dashboard (#97)
    * tyrus dashboard and visualizations
    
    * linting and black
    
    * merge conflict fix
    
    * fixed typo in requirements
    
    * linting round 2
    
    * linting round 3
    
    * undid change to rename limeresults.map to limeresults.get_saliencies
    
    * fixed dual output bug, added bokeh to pyproject
    
    * linting
    
    * return plots either way

--- src/trustyai/utils/_visualisation.py
+++ src/trustyai/utils/_visualisation.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 import pandas as pd
 from pandas.io.formats.style import Styler
 
+
 # pylint: disable=too-few-public-methods
 class ExplanationVisualiser(ABC):
     """Abstract class for explanation visualisers"""
@@ -16,9 +17,38 @@ class ExplanationVisualiser(ABC):
         """Visualise the styled dataframe"""
 
 
+# HTML FORMAT FUNCTIONS ============================================================================
+def bold_green_html(content):
+    """Format the content string as a bold, green html object"""
+    return '<b style="color:{};">{}</b>'.format(
+        DEFAULT_STYLE["positive_primary_colour"], content
+    )
+
+
+def bold_red_html(content):
+    """Format the content string as a bold, red html object"""
+    return '<b style="color:{};">{}</b>'.format(
+        DEFAULT_STYLE["negative_primary_colour"], content
+    )
+
+
+def output_html(content):
+    """Format the content string as a bold object in TrustyAI purple, used for
+    Tyrus output displays"""
+    return '<b style="color:#a64d79;background-color:#fff;">{}</b>'.format(content)
+
+
+def feature_html(content):
+    """Format the content string as a bold object in black, used for
+    Tyrus feature displays"""
+    return '<b style="color:#000000;background-color:#fff;">{}</b>'.format(content)
+
+
 DEFAULT_STYLE = {
     "positive_primary_colour": "#13ba3c",
+    "positive_primary_colour_faded": "#88dc9d",
     "negative_primary_colour": "#ee0000",
+    "negative_primary_colour_faded": "#f67f7f",
     "neutral_primary_colour": "#ffffff",
 }
 
