bc4ec92bc865ab962c5bf0e89772166d187a930b
Rui Vieira <ruivieira@users.noreply.github.com>
2022-12-01 12:56:53 +0000

    Add full text feature builder (#125)

---
+++ src/trustyai/utils/text.py
@@ -0,0 +1,13 @@
+"""Utility methods for text data handling"""
+from typing import List, Callable
+
+from jpype import _jclass
+
+
+def tokenizer(function: Callable[[str], List[str]]):
+    """Post-process outputs of a Python tokenizer function"""
+
+    def wrapper(_input: str):
+        return _jclass.JClass("java.util.Arrays").asList(function(_input))
+
+    return wrapper
