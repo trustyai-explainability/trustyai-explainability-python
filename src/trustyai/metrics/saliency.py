1237999e6b28e752d00f1b469bd03a92599ec481
Tommaso Teofili <tteofili@redhat.com>
2022-12-06 11:46:24 +0100

    FAI-416 - pylint related fixes

--- src/trustyai/metrics/saliency.py
+++ src/trustyai/metrics/saliency.py
@@ -1,7 +1,8 @@
+# pylint: disable = import-error
+"""Saliency evaluation metrics"""
 from org.apache.commons.lang3.tuple import (
     Pair as _Pair,
 )
-
 
 from trustyai.model import simple_prediction
 from trustyai.explainers.lime import LimeExplainer
@@ -12,6 +13,25 @@ from . import ExplainabilityMetrics
 
 
 def impact_score(model, pred_input, explainer, k, is_model_callable=False):
+    """
+    Parameters
+    ----------
+    model: trustyai.PredictionProvider
+        the model used to generate predictions
+    pred_input: trustyai.PredictionInput
+        the input to the model
+    explainer: Union[trustyai.explainers.LimeExplainer, trustyai.explainers.SHAPExplainer]
+        the explainer to evaluate
+    k: int
+        the number of top important features
+    is_model_callable: bool
+        whether to directly use model function call or use the predict method
+
+    Returns
+    -------
+    :float:
+        impact score metric
+    """
     if is_model_callable:
         output = model(pred_input)
     else:
@@ -24,13 +44,49 @@ def impact_score(model, pred_input, explainer, k, is_model_callable=False):
 
 
 def mean_impact_score(explainer, model, data, is_model_callable=False, k=2):
+    """
+    Parameters
+    ----------
+    explainer: Union[trustyai.explainers.LimeExplainer, trustyai.explainers.SHAPExplainer]
+        the explainer to evaluate
+    model: trustyai.PredictionProvider
+        the model used to generate predictions
+    data: list[list[trustyai.model.Feature]]
+        the inputs to calculate the metric for
+    is_model_callable: bool
+        whether to directly use model function call or use the predict method
+    k: int
+        the number of top important features
+
+    Returns
+    -------
+    :float:
+        the mean impact score metric across all inputs
+    """
     m_is = 0
     for features in data:
         m_is += impact_score(model, features, explainer, k, is_model_callable=is_model_callable)
-    return m_is/len(data)
+    return m_is / len(data)
 
 
 def classification_fidelity(explainer, model, inputs, is_model_callable=False):
+    """
+    Parameters
+    ----------
+    explainer: Union[trustyai.explainers.LimeExplainer, trustyai.explainers.SHAPExplainer]
+        the explainer to evaluate
+    model: trustyai.PredictionProvider
+        the model used to generate predictions
+    inputs: list[list[trustyai.model.Feature]]
+        the inputs to calculate the metric for
+    is_model_callable: bool
+        whether to directly use model function call or use the predict method
+
+    Returns
+    -------
+    :float:
+        the classification fidelity metric
+    """
     pairs = []
     for c_input in inputs:
         if is_model_callable:
@@ -44,13 +100,37 @@ def classification_fidelity(explainer, model, inputs, is_model_callable=False):
 
 
 def local_saliency_f1(output_name, model, explainer, distribution, k, chunk_size):
-    local_explainer = None
+    """
+    Parameters
+    ----------
+    output_name: str
+        the name of the output to calculate the metric for
+    model: trustyai.PredictionProvider
+        the model used to generate predictions
+    explainer: Union[trustyai.explainers.LIMEExplainer, trustyai.explainers.SHAPExplainer,
+                trustyai.explainers.LocalExplainer]
+        the explainer to evaluate
+    distribution: org.kie.trustyai.explainability.model.PredictionInputsDataDistribution
+        the data distribution to fetch the inputs from
+    k: int
+        the number of top important features
+    chunk_size: int
+        the chunk of inputs to fetch fro the distribution
+
+    Returns
+    -------
+    :float:
+        the local saliency f1 metric
+    """
     if isinstance(explainer, LimeExplainer):
         local_explainer = LocalExplainer(explainer._explainer)
     elif isinstance(explainer, SHAPExplainer):
         local_explainer = LocalExplainer(explainer._explainer)
+    elif isinstance(explainer, LocalExplainer):
+        local_explainer = explainer
     else:
-        raise ValueError("wrong explaienr type")
+        raise ValueError(f"Wrong explainer type '{explainer}', "
+                         f"expected one of [LimeExplainer, SHAPExplainer, LocalExplainer]")
 
-    return ExplainabilityMetrics.getLocalSaliencyF1(output_name, model, local_explainer, distribution, k, chunk_size)
-
+    return ExplainabilityMetrics.getLocalSaliencyF1(output_name, model, local_explainer,
+                                                    distribution, k, chunk_size)
