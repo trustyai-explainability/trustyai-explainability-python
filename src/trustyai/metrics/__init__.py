4ec8e824933dca43ca66d7e2233b53336f1821e2
Tommaso Teofili <tteofili@redhat.com>
2022-12-07 09:41:51 +0100

    FAI-416 - restored correct requirements-dev.txt, fixed import for metrics package

--- src/trustyai/metrics/__init__.py
+++ src/trustyai/metrics/__init__.py
@@ -1,7 +1,7 @@
 # pylint: disable = import-error, invalid-name, wrong-import-order, no-name-in-module
 """General model classes"""
 from trustyai import _default_initializer  # pylint: disable=unused-import
-from org.kie.trustyai.explainability.utils import (
+from org.kie.trustyai.explainability.metrics import (
     ExplainabilityMetrics as _ExplainabilityMetrics,
 )
 
