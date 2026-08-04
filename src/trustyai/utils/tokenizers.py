426a31a6d443a05fadc2e9ecc91014ca38f0181a
Rui Vieira <ruidevieira@googlemail.com>
2026-08-05 00:00:31 +0100

    fix: suppress pylint warnings on OpenNLPTokenizer

--- src/trustyai/utils/tokenizers.py
+++ src/trustyai/utils/tokenizers.py
@@ -8,5 +8,6 @@ from opennlp.tools.tokenize import SimpleTokenizer as _SimpleTokenizer
 CommonsStringTokenizer = _StringTokenizer
 
 
-def OpenNLPTokenizer():
+def OpenNLPTokenizer():  # pylint: disable=invalid-name
+    """Return the OpenNLP SimpleTokenizer singleton (2.x API)."""
     return _SimpleTokenizer.INSTANCE
